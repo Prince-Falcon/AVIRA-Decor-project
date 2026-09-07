4f936a9332bb68dcde4ca5da8725d794c1cb30c1
Prince-Falcon <ammbahmani@gmail.com>
2026-09-07 17:35:33 +0330

    ض

--- script.js
+++ script.js
@@ -401,12 +401,10 @@ const collectionsProducts = {
     },
     movie: {
         title: "مجموعه Movie",
-        items: [
-            { id: 701, title: "طرح Avengers:Endgame", price: "570,000 Toman", img: "assets/images/collection52.png" },
+        items: [,
             { id: 702, title: "طرح Better Call Saul", price: "600,000 Toman", img: "assets/images/collection53.png" },
             { id: 703, title: "طرح Peaky blinders", price: "590,000 Toman", img: "assets/images/collection54.png" },
             { id: 704, title: "طرح The mentalist", price: "590,000 Toman", img: "assets/images/collection55.png" },
-            { id: 705, title: "طرح BreakingBad", price: "600,000 Toman", img: "assets/images/collection56.png" }
         ]
     }
 };
